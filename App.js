b476d75d9532f36d290ca366a26015ef91bb39c1
Atik Bin Hashmee <atikhashmee6235@gmail.com>
2020-04-15 20:08:25 +0600

    icons added

--- App.js
+++ App.js
@@ -1,114 +1,103 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import * as React from 'react';
+import {View, Text, Linking} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
+import {MaterialCommunityIcons} from 'react-native-vector-icons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+const FormView = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={{backgroundColor: 'green', flex: 1}}>
+      <Text>Hell oworld form View</Text>
+    </View>
+  );
+};
+const ListView = () => {
+  return (
+    <View style={{backgroundColor: 'red', flex: 1}}>
+      <Text>Hell oworld List View</Text>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const MainHomePage = () => {
+  const Tab = createBottomTabNavigator();
+  return (
+    <View style={{flex:1}}>
+    <View style={{height:10,backgroundColor:'blue',width:100}}></View>
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}>
+      <Tab.Screen
+        name="Form"
+        component={FormView}
+        options={{
+          tabBarLabel: 'Form',
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="List"
+        component={ListView}
+        options={{
+          tabBarLabel: 'List',
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="alarm-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+    </View>
+  );
+};
 
-export default App;
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <View
+        style={{
+          backgroundColor: '#f50057',
+          height: 140,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Text style={{color: 'white', fontSize: 30}}>Header</Text>
+      </View>
+      <DrawerItem
+        label="IconHeader"
+        onPress={() => Linking.openUrl('https://google.com')}
+      />
+      <DrawerItem
+        label="Help"
+        onPress={() => Linking.openUrl('https://google.com')}
+      />
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+}
+
+export default function App() {
+  const Drawer = createDrawerNavigator();
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        drawerType={'front'}
+        drawerStyle={{
+          width: 240,
+        }}
+        drawerContent={props => <CustomDrawerContent {...props} />}>
+        <Drawer.Screen name="Home" component={MainHomePage} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
